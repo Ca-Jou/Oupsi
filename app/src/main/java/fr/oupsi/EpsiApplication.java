b9f67380916b1952f659706162ed346ee48f88b7
Johanna Jato <johanna.jato@gmail.com>
2021-03-18 17:23:16 +0100

    homePage

---
+++ app/src/main/java/fr/oupsi/EpsiApplication.java
@@ -0,0 +1,11 @@
+package fr.oupsi;
+
+import android.app.Application;
+
+public class EpsiApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
